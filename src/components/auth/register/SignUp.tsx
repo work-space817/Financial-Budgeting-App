fe6710572547e9c005d186c503994ac3ac59e8f0
work-space817 <workspace817@gmail.com>
2023-07-05 18:52:15 +0300

    hot fix 05.07

--- src/components/auth/register/SignUp.tsx
+++ src/components/auth/register/SignUp.tsx
@@ -17,50 +17,122 @@ const SignUp = () => {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState<IRegisterError>();
 
-  const onSubmit = async (e: any) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+    setEmail(e.target.value);
+    setName(e.target.value);
+    setConfirmPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("Formik Submit Form to Server", e);
+
     try {
-      const result = await http.post("api/account/register", e);
-      console.log("Result server good", result);
-    } catch (err: any) {
-      const error = err.response.data.errors as IRegisterError;
-      if (error.email) {
-        setFieldError("email", error.email[0]);
-        return;
-      }
-      setError(error);
-      console.log("Bad request", err);
+      const formData = new FormData();
+      formData.append("email", email);
+      formData.append("name", name);
+      formData.append("password", password);
+      formData.append("confirm", confirmPassword);
+      // Виконати POST-запит до серверу на локальному хості 5000
+      const response = await http.post("api/account/register", formData);
+
+      // Обробити успішну відповідь від серверу
+      console.log(response.data);
+
+      // Очистити поля після відправки форми
+      setEmail("");
+      setName("");
+      setPassword("");
+      setConfirmPassword("");
+    } catch (error) {
+      // Обробити помилку, якщо сталася
+      console.error(error);
     }
   };
 
   return (
     <>
-      <form>
-        <Input
+      <form onSubmit={handleSubmit}>
+        {/* <Input
           type="email"
           label="Email address"
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={handleChange}
           required
           placeholder="Email address"
+          field={""}
+        />
+        <Input
+          type="text"
+          label="Name"
+          value={name}
+          onChange={handleChange}
+          required
+          placeholder="Name"
           field={""}
         />
         <Input
           type="password"
           label="Create password"
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={handleChange}
           required
           placeholder="Password"
           field={""}
         />
+        <Input
+          type="password"
+          label="confirm password"
+          value={confirmPassword}
+          onChange={handleChange}
+          required
+          placeholder="confirm Password"
+          field={""}
+        /> */}
+        <div className="mb-3">
+          <label>
+            email:
+            <input
+              type="email"
+              value={password}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            name:
+            <input
+              type="text"
+              value={password}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
 
-        <button type="submit" onClick={onSubmit}>
-          Sign up
-        </button>
+        <button type="submit">Sign up</button>
       </form>
 
       <p>
@@ -71,6 +143,3 @@ const SignUp = () => {
 };
 
 export default SignUp;
-function setFieldError(arg0: string, arg1: string) {
-  throw new Error("Function not implemented.");
-}
